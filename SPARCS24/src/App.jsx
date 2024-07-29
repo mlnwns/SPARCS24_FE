f7b23d5986002420ff4051a5be65b101e0a103f2
mlnwns <mlnwns@naver.com>
2024-07-29 14:58:44 +0900

    feat: 로그인 경로 추가

--- SPARCS24/src/App.jsx
+++ SPARCS24/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import NaverMap from "./pages/NaverMap";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
-      <Route path="map" element={<NaverMap />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/map" element={<NaverMap />} />
     </Routes>
   );
 }
