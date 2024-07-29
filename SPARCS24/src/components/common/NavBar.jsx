f2aae9d021d7e1ecfc5e852772c33d4269675f1b
mlnwns <mlnwns@naver.com>
2024-07-29 21:00:40 +0900

    feat: NavBar 디자인 마크업

--- SPARCS24/src/components/common/NavBar.jsx
+++ SPARCS24/src/components/common/NavBar.jsx
@@ -1,11 +1,19 @@
 import styled from "styled-components";
+import { IoIosSearch } from "react-icons/io";
 
-const NavBarContainer = styled.nav`
+const UpNavBarContainer = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1rem 2rem;
-  background-color: #f8f9fa;
+  padding: 3rem 5rem 0 5rem;
+  font-size: 0.9rem;
+`;
+
+const DownNavBarContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 5rem;
 `;
 
 const NavList = styled.ul`
@@ -16,7 +24,7 @@ const NavList = styled.ul`
 `;
 
 const NavItem = styled.li`
-  margin: 0 1.3rem;
+  margin: 0 1.65rem;
 
   &:first-child {
     margin-left: 0;
@@ -39,28 +47,83 @@ const NavLink = styled.a`
 
 const NavBar = () => {
   return (
-    <NavBarContainer>
-      <NavList>
-        <NavItem>
-          <NavLink href="#">자기소개서 작성</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">문서 작성</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">AI 연결</NavLink>
-        </NavItem>
-      </NavList>
-      <NavList>
-        <NavItem>
-          <NavLink href="#">검색</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">마이페이지</NavLink>
-        </NavItem>
-      </NavList>
-    </NavBarContainer>
+    <>
+      <UpNavBarContainer>
+        <LogoWrapper>
+          <img src="/logo.png" alt="SPARCS 24th" />
+        </LogoWrapper>
+        <NavList>
+          <NavItem>
+            <NavLink href="#">로그인/회원가입</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">고객센터</NavLink>
+          </NavItem>
+        </NavList>
+      </UpNavBarContainer>
+      <DownNavBarContainer>
+        <NavList>
+          <NavItem>
+            <NavLink href="#">전체 카테고리</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">교육 강의</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">자기소개 작성</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">문서 작성</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">하모니 찾기</NavLink>
+          </NavItem>
+        </NavList>
+        <NavList>
+          <Search>
+            <Input placeholder="하모니를 찾아보세요" type="text" />
+            <IconSpace>
+              <IoIosSearch />
+            </IconSpace>
+          </Search>
+        </NavList>
+      </DownNavBarContainer>
+    </>
   );
 };
 
 export default NavBar;
+
+const LogoWrapper = styled.div`
+  img {
+    width: 4rem;
+  }
+`;
+
+const Input = styled.input`
+  width: 12.375rem;
+  height: 2.1875rem;
+  padding: 0.25rem 2rem 0.25rem 0.625rem;
+  border: 0.0625rem solid #fff;
+  background-color: #ececec;
+  box-sizing: border-box;
+  border-radius: 0.5rem;
+  outline: none;
+  position: relative;
+  &::placeholder {
+    color: #b7b7b7;
+  }
+`;
+
+const Search = styled.div`
+  position: relative;
+  height: 2.1875rem;
+  margin: auto 0 auto 30.625rem;
+`;
+
+const IconSpace = styled.div`
+  position: absolute;
+  right: 9px;
+  top: 9px;
+  cursor: pointer;
+`;
