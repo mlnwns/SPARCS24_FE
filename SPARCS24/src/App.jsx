30117cff2766d30206b6e7478ddbc2fb22fd0da7
mlnwns <mlnwns@naver.com>
2024-07-30 00:33:37 +0900

    faet: senior 경로 추가

--- SPARCS24/src/App.jsx
+++ SPARCS24/src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import NaverMap from "./pages/NaverMap";
 import LoginPage from "./pages/LoginPage";
+import SeniorMainPage from "./pages/SeniorMainPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/senior" element={<SeniorMainPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/map" element={<NaverMap />} />
     </Routes>
