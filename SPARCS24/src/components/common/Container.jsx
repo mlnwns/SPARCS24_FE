3815ecb48f5d8e09756c885f0c262a9911dbcb56
mlnwns <mlnwns@naver.com>
2024-07-29 21:00:51 +0900

    feat: Container 두 가지 유형으로 분리

--- SPARCS24/src/components/common/Container.jsx
+++ SPARCS24/src/components/common/Container.jsx
@@ -1,23 +1,29 @@
 import styled from "styled-components";
 
-const Container = ({ children, ...props }) => {
-  return <StyledContainer {...props}>{children}</StyledContainer>;
+const Container = ({ children, main = false, ...props }) => {
+  return (
+    <StyledContainer main={main} {...props}>
+      {children}
+    </StyledContainer>
+  );
 };
 
 export default Container;
 
 const StyledContainer = styled.div`
   width: 100%;
-  max-width: 1200px; // 최대 너비 설정
-  margin: 0 auto; // 중앙 정렬
-  padding: 5rem 2rem;
-  box-sizing: border-box; // 패딩을 너비에 포함
+  margin: 0 auto; /* 중앙 정렬 */
+  padding: ${(props) =>
+    props.main ? "2rem 5rem 5rem 5rem" : "10rem 7rem"}; /* 여백 조정 */
+  box-sizing: border-box; /* 패딩을 너비에 포함 */
 
   @media (max-width: 768px) {
-    padding: 0 15px;
+    padding: ${(props) =>
+      props.main ? "1rem" : "0 15px"}; /* 모바일에서 여백 조정 */
   }
 
   @media (max-width: 480px) {
-    padding: 0 10px;
+    padding: ${(props) =>
+      props.main ? "0.5rem" : "0 10px"}; /* 더 작은 화면에서 여백 조정 */
   }
 `;
