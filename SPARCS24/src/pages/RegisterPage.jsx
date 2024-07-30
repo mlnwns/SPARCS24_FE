fad2d14e6b47d139ae5c2c1b5ad9a59648e4a3f0
mlnwns <mlnwns@naver.com>
2024-07-30 21:17:08 +0900

    feat: 회원가입 페이지 마크업

--- SPARCS24/src/pages/RegisterPage.jsx
+++ SPARCS24/src/pages/RegisterPage.jsx
@@ -1,82 +1,126 @@
-import { useState } from "react";
-import styled from "styled-components";
 import Container from "../components/common/Container";
-import InputComponent from "../components/common/InputComponent";
-import NavBar from "../components/common/NavBar";
-
-const ErrorMessage = styled.div`
-  color: red;
-  font-size: 0.875rem;
-  margin-top: 0.3rem;
-  margin-bottom: 1.5rem;
-`;
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
 
-  const validateUsername = (username) => {
-    const regex = /^[a-zA-Z0-9]+$/; // 영문자와 숫자만 허용
-    return regex.test(username);
+  const handleParentRegister = () => {
+    navigate("/register/parent");
   };
 
-  const validatePassword = (password) => {
-    return password.length >= 8 && password.length <= 20; // 8자 이상 20자 이하
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newErrors = {};
-    if (!validateUsername(username)) {
-      newErrors.username = "아이디는 영문자와 숫자만 입력 가능합니다.";
-    }
-    if (!validatePassword(password)) {
-      newErrors.password = "비밀번호는 8자 이상 20자 이하여야 합니다.";
-    }
-    setErrors(newErrors);
-
-    if (Object.keys(newErrors).length === 0) {
-      // 유효성 검사 통과 시 처리 로직
-      console.log("Form submitted");
-    }
+  const handleSeniorRegister = () => {
+    navigate("/register/senior");
   };
 
   return (
-    <div>
-      <NavBar />
-      <Container>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <InputComponent
-              label="아이디"
-              name="username"
-              size="lg"
-              placeholder="아이디를 입력하세요"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            {errors.username && <ErrorMessage>{errors.username}</ErrorMessage>}
-          </div>
-          <div>
-            <InputComponent
-              label="비밀번호"
-              type="password"
-              name="password"
-              size="lg"
-              placeholder="비밀번호를 입력하세요"
-              showToggle={true}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              maxLength={20} // 최대 20자로 제한
-            />
-            {errors.password && <ErrorMessage>{errors.password}</ErrorMessage>}
-          </div>
-          <button type="submit">로그인</button>
-        </form>
-      </Container>
-    </div>
+    <StyledContainer>
+      <TitleContainer>
+        <Title>하모니 케어</Title>
+        <Subtitle>안녕하세요. 하모니케어 회원가입을 환영합니다.</Subtitle>
+      </TitleContainer>
+      <CardContainer>
+        <Card>
+          <CardTitle>부모님</CardTitle>
+          <ButtonGroup>
+            <RegisterButton onClick={handleParentRegister}>
+              가입하기
+            </RegisterButton>
+            <SNSButton>카카오로 가입하기</SNSButton>
+          </ButtonGroup>
+        </Card>
+        <Card>
+          <CardTitle>시니어</CardTitle>
+          <ButtonGroup>
+            <RegisterButton onClick={handleSeniorRegister}>
+              가입하기
+            </RegisterButton>
+            <SNSButton>카카오로 가입하기</SNSButton>
+          </ButtonGroup>
+        </Card>
+      </CardContainer>
+    </StyledContainer>
   );
 };
 
 export default RegisterPage;
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const TitleContainer = styled.div`
+  text-align: center;
+  margin-bottom: 4rem;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 700;
+  margin-bottom: 1rem;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 1.2rem;
+  font-weight: 400;
+  color: #666;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 4rem;
+`;
+
+const Card = styled.div`
+  width: 300px;
+  height: 400px;
+  border-radius: 10px;
+  display: flex;
+  background-color: #f5f5f5;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem;
+`;
+
+const CardTitle = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-top: 3rem;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-bottom: 2rem;
+`;
+
+const RegisterButton = styled.button`
+  padding: 1rem 7rem;
+  font-size: 1rem;
+  background-color: #d2d2d2;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #a8a8a8;
+  }
+`;
+
+const SNSButton = styled(RegisterButton)`
+  padding: 1rem 5.3rem;
+  background-color: #ffeb00;
+  color: #000000;
+
+  &:hover {
+    background-color: #ffcc00;
+  }
+`;
