564dc75a2d6e1870ee07f0a65f35a9e20bed9a85
mlnwns <mlnwns@naver.com>
2024-07-30 21:25:10 +0900

    feat: 회원가입 경로 추가

--- SPARCS24/src/App.jsx
+++ SPARCS24/src/App.jsx
@@ -3,6 +3,9 @@ import MainPage from "./pages/MainPage";
 import NaverMap from "./pages/NaverMap";
 import LoginPage from "./pages/LoginPage";
 import SeniorMainPage from "./pages/SeniorMainPage";
+import RegisterPage from "./pages/RegisterPage";
+import ParentRegisterPage from "./pages/ParentRegisterPage";
+import SeniorRegisterPage from "./pages/SeniorRegisterPage";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Route path="/senior" element={<SeniorMainPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/map" element={<NaverMap />} />
+      <Route path="register" element={<RegisterPage />} />
+      <Route path="/register/parent" element={<ParentRegisterPage />} />
+      <Route path="/register/senior" element={<SeniorRegisterPage />} />
     </Routes>
   );
 }
