859c9ba62a2e8e80994abee9497dc314a02802a7
mlnwns <mlnwns@naver.com>
2024-08-01 04:39:30 +0900

    chore: 배너 순서 변경

--- SPARCS24/src/pages/SeniorMainPage.jsx
+++ SPARCS24/src/pages/SeniorMainPage.jsx
@@ -2,16 +2,15 @@ import NavBar from "../components/common/NavBar";
 import SeniorContainer from "../components/common/SeniorContainer";
 import SwiperComponent from "../components/common/SwiperComponent";
 import Subtitle from "../components/mainpage/Subtitle";
-import Footer from "../components/common/Footer";
 import TabComponent from "../components/mainpage/TabComponent";
 import Stories from "../components/mainpage/Stories";
 import Lectures from "../components/mainpage/Lectures";
 import Questions from "../components/mainpage/Questions";
 
 const images = [
-  { src: "/SeniorBanner1.png", alt: "Image 1" },
-  { src: "/SeniorBanner2.png", alt: "Image 2" },
+  { src: "/SeniorBanner2.png", alt: "Image 1" },
   { src: "/SeniorBanner3.png", alt: "Image 2" },
+  { src: "/SeniorBanner1.png", alt: "Image 2" },
 ];
 
 const SeniorMainPage = () => {
@@ -28,7 +27,11 @@ const SeniorMainPage = () => {
         <Subtitle senior="true">하모니들이 자주 묻는 질문</Subtitle>
         <Questions />
       </SeniorContainer>
-      <Footer />
+      <img
+        src="/Footer.png"
+        alt="Footer"
+        style={{ marginTop: "8rem", width: "100%" }}
+      />
     </>
   );
 };
