a9da52d2895e0f58e09b1bd6cd6ab9efb6b13078
mlnwns <mlnwns@naver.com>
2024-07-31 14:58:22 +0900

    style: Profile컴포넌트 CSS변경

--- SPARCS24/src/components/mainpage/Profiles.jsx
+++ SPARCS24/src/components/mainpage/Profiles.jsx
@@ -33,8 +33,17 @@ const ProfileInfo = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
-  padding: 40px;
-  background: linear-gradient(to top, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0));
+  padding: 20px;
+  background: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.7) 0%,
+    rgba(0, 0, 0, 0.5) 50%,
+    rgba(0, 0, 0, 0) 100%
+  );
+  height: 30%; // 그림자 효과의 높이를 증가
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
   color: white;
 `;
 
@@ -51,10 +60,26 @@ const Name = styled.p`
 
 const Profiles = () => {
   const profiles = [
-    { src: "/img2.png", address: "성북구 정릉동", name: "김*경 하모니•58세" },
-    { src: "/img2.png", address: "강남구 역삼동", name: "이*희 하모니•62세" },
-    { src: "/img2.png", address: "마포구 합정동", name: "박*철 하모니•55세" },
-    { src: "/img2.png", address: "서초구 반포동", name: "최*영 하모니•60세" },
+    {
+      src: "/Grandma1.png",
+      address: "성북구 정릉동",
+      name: "김*경 하모니•58세",
+    },
+    {
+      src: "/Grandma2.png",
+      address: "강남구 역삼동",
+      name: "이*희 하모니•62세",
+    },
+    {
+      src: "/Grandma3.png",
+      address: "마포구 합정동",
+      name: "박*철 하모니•55세",
+    },
+    {
+      src: "/Grandma4.png",
+      address: "서초구 반포동",
+      name: "최*영 하모니•60세",
+    },
   ];
 
   return (
